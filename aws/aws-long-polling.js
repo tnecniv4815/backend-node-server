9bb00912a062e13f3e653c7acca30a325a3d087c
Vincent <va543567@gmail.com>
2018-11-20 23:14:07 +0800

    save article to db and S3

--- aws/aws-long-polling.js
+++ aws/aws-long-polling.js
@@ -3,6 +3,11 @@
 const cron = require('node-cron');
 const AWS = require('aws-sdk');
 const _ = require('lodash');
+const request = require('request');
+const moment = require('moment');
+const Url = require('url');
+const Path = require('path');
+const uuidv5 = require('uuid/v5');
 
 const Article = require('./../schema/Article');
 
@@ -12,6 +17,8 @@ const s3Articles = process.env.AWS_S3_FOLDER_ARTICLE;
 const s3ArticleLinks = process.env.AWS_S3_FOLDER_ARTICLE_LINK;
 const s3ArticleDetail = process.env.AWS_S3_FOLDER_ARTICLE_DETAIL;
 const s3Media = process.env.AWS_S3_FOLDER_MEDIA;
+
+const DATE_FORMAT = 'YYYY-MM-DD_hh:mm:ss:SSS';
 
 
 const task = cron.schedule('*/5 * * * * *', scheduleTask, {
@@ -55,7 +62,29 @@ function scheduleTask() {
 
     monitorS3Bucket();
 
-    // monitorSQS(articleQueueUrl);
+
+    const imgUrl = 'https://p2.bahamut.com.tw/S/2KU/59/65c6ec3792ecdc239bc5000c8512ztr5.JPG';
+
+
+
+    // fileUrlToData(imgUrl)
+    //     .then(body => {
+    //         console.log('fileUrlToData body: ', body.length);
+    //
+    //         createObjectInS3Bucket(bucketName, s3Media, 'good.jpg', body)
+    //             .then(data => {
+    //                 console.log('createObjectInS3Bucket_data: ', data);
+    //             })
+    //             .catch(err => {
+    //                 console.log('createObjectInS3Bucket_err: ', err);
+    //             });
+    //
+    //     })
+    //     .catch(err => {
+    //         console.log('fileUrlToData err: ', err);
+    //     })
+
+
 }
 
 /**
@@ -77,17 +106,61 @@ function monitorS3Bucket() {
                     getS3Object(s3ResultObj.Key)
                         .then(s3DataObj => {
                             const dataStr = s3DataObj.Body.toString();
-                            console.log('getS3Object: ', dataStr);
+                            // console.log('getS3Object: ', dataStr);
 
                             const articleObj = JSON.parse(dataStr);
                             if (!_.isNull(articleObj)) {
-                                // insertArticle(articleObj)
-                                //     .then(insertResult => {
-                                //         console.log('data insert success, title: ', articleObj.title);
-                                //     })
-                                //     .catch(error => {
-                                //         console.log('Insert to db failure: ', error);
-                                //     })
+
+                                const imgUrl = articleObj.thumbnail;
+                                const filenameWithExt = uuidv5(imgUrl, uuidv5.URL) + '.' + getExtensionFromurl(imgUrl);
+
+                                const thumbnailUrl = s3Media + '/' + filenameWithExt;
+
+                                articleObj.thumbnailUrl = thumbnailUrl;
+
+                                // console.log('\n\n thumbnailUrl: ', filenameWithExt, '\n\n');
+
+                                fileUrlToData(imgUrl)
+                                    .then(body => {
+                                        // console.log('fileUrlToData body: ', body.length);
+
+                                        findOneArticle({ title: articleObj.title })
+                                            .then(findArticleObjResult => {
+                                                if (findArticleObjResult == null) {
+
+                                                    // save image to S3
+                                                    createObjectInS3Bucket(bucketName, s3Media, filenameWithExt, body)
+                                                        .then(data => {
+                                                            console.log('thumbnail saved to S3: ', thumbnailUrl);
+                                                        })
+                                                        .catch(err => {
+                                                            console.log('thumbnail save failure: ', err);
+                                                        });
+
+
+                                                    // insert data to db
+                                                    insertArticle(articleObj)
+                                                        .then(insertResult => {
+                                                            console.log('article insert to db success, title: ', articleObj.title);
+                                                        })
+                                                        .catch(error => {
+                                                            console.log('Article insert to db failure: ', error);
+                                                        })
+                                                }
+                                            })
+                                            .catch(err => {
+                                                console.log('find article from db failure: ', err);
+                                            });
+
+
+
+                                    })
+                                    .catch(err => {
+                                        console.log('fileUrlToData err: ', err);
+                                    })
+
+
+
                             }
 
                         })
@@ -105,6 +178,48 @@ function monitorS3Bucket() {
 
 }
 
+/**
+ * Common
+ */
+function getCurrentTime() {
+    const datetime = moment().format(DATE_FORMAT);
+
+    return datetime;
+}
+
+function getExtensionFromurl(url) {
+    let extension = '';
+
+    if (!_.isNull(url) && url !== '') {
+        extension = Path.extname(Url.parse(url).pathname).replace('.', '');
+    }
+
+    // console.log('getExtensionFromurl: ', extension);
+    return extension;
+}
+
+function fileUrlToData(url) {
+    // let data = null;
+    // if (url !== '') {
+    //     request.get(url, (err, response, body) => {
+    //
+    //     });
+    // }
+    // return data;
+    return new Promise((resolve, reject) => {
+        if (url !== '') {
+            request.get({
+                url: url,
+                encoding: null
+            }, (err, response, body) => {
+                if (err) {
+                    reject(err);
+                }
+                resolve(body);
+            });
+        }
+    });
+}
 
 /**
  * DB function
@@ -115,7 +230,7 @@ function insertArticle(articleObject) {
         if (articleObject) {
             Article.create({
                 title: articleObject.title,
-                thumbnail: articleObject.thumbnail,
+                thumbnail: articleObject.thumbnailUrl,
             }, (err, result) => {
                 if (err) {
                     reject(err);
@@ -123,6 +238,23 @@ function insertArticle(articleObject) {
                     resolve(result);
                 }
             })
+        } else {
+            reject(new Error('Article Object is null'));
+        }
+    });
+}
+
+function findOneArticle(articleObject) {
+    return new Promise((resolve, reject) => {
+        if (articleObject) {
+            Article.findOne({ title: articleObject.title })
+                .then(result => {
+                    if (result) {
+                        resolve(result);
+                    } else {
+                        resolve(null);
+                    }
+                })
         } else {
             reject(new Error('Article Object is null'));
         }
@@ -175,6 +307,15 @@ function getS3Object(key) {
             resolve(data);
         });
     });
+}
+
+function createObjectInS3Bucket(bucketName, directory, fileNameWithExt, body) {
+    const params = {
+        Bucket: bucketName,
+        Key: directory + '/' + fileNameWithExt,
+        Body: body
+    };
+    return s3.upload(params).promise();
 }
 
 
